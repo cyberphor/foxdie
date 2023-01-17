1d22f1fa5da8f0d2676700236266e47240fc1c22
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-01-17 06:00:19 -0800

    2023-01-17, 0600

--- foxdie/servers.py
+++ foxdie/servers.py
@@ -14,11 +14,19 @@ class Server:
         self.plug = (str(self.address), self.port)
             
     def start(self):
-        try:   
-            self.socket = socket(AF_INET, SOCK_STREAM)
-            self.socket.bind(self.plug) 
-            self.socket.listen(2)
-            print(f"[FOXDIE: {self.port}] Listening.")
+        def stop(self):
+            self.socket.close()
+            print(f"[FOXDIE: {self.port}] Stopped listening.")
+            for thread in self.threads:
+                thread.join()
+                print(f"[FOXDIE: {thread.name}] Disconnected.")
+            return False
+
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind(self.plug) 
+        self.socket.listen(2)
+        print(f"[FOXDIE: {self.port}] Listening.")
+        try:
             while True:
                 agent_connection, agent_plug = self.socket.accept()
                 agent_port = agent_plug[1]
@@ -31,28 +39,19 @@ class Server:
                 thread.start()
                 self.threads.append(thread)
                 print(f"[FOXDIE: {thread.name}] Connected.")
-        except KeyboardInterrupt:
-            self.stop()
-
-    def stop(self):
-        self.socket.close()
-        print(f"[FOXDIE: {self.port}] Stopped listening.")
-        for thread in self.threads:
-            thread.join()
-            print(f"[FOXDIE: {thread.name}] Disconnected.")
-        exit()
+        except:
+            stop()
 
     def handle(self, agent_connection, agent_port):
-        while True:
-            try:
-                data = agent_connection.recv(1024).decode()
-                if data.lower() == "exit":
-                    raise Exception(f"[FOXDIE: {agent_port}] Disconnected.")
-                else:
-                    print(f"[FOXDIE: {agent_port}] {data}")
-                    response = input(f"[FOXDIE: {self.port}] ")
-                    agent_connection.send(response.encode())
-            except:
-                agent_connection.close()
-                self.threads[agent_port].join
-                return False
+        try:
+            data = agent_connection.recv(1024).decode()
+            if data.lower() == "exit":
+                raise Exception(f"[FOXDIE: {agent_port}] Disconnected.")
+            else:
+                print(f"[FOXDIE: {agent_port}] {data}")
+                response = input(f"[FOXDIE: {self.port}] ")
+                agent_connection.send(response.encode())
+        except:
+            agent_connection.close()
+            self.threads[agent_port].join
+            return False
