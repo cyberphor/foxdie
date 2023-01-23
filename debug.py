fe65ac5e29d286c6a2c64cce99b851b5fcdb6c21
Victor Fernandez III <48382964+cyberphor@users.noreply.github.com>
2023-01-23 08:33:51 -0500

    2023-01-23, 0833

--- debug.py
+++ debug.py
@@ -1,64 +1,72 @@
 from dataclasses import dataclass, field
 from ipaddress import IPv4Address
-from threading import Event, Thread
+from threading import Thread
 from time import sleep, time
+from typing import Union
 from socket import AF_INET, SOCK_STREAM, socket
 
+time_to_stop = False
+
 @dataclass
-class Server:
-    ip: IPv4Address = field(default = "127.0.0.1") 
-    port: int = field(default = 80)
-
-    def __post_init__(self):
-        self.address = (str(self.ip), self.port)
-        self.done = False
-
-    def handler(self, agent_socket: socket, agent_port: int):
+class Handler:
+    def start(self, agent_socket: socket, agent_port: int):
         print(f"[FOXDIE: {agent_port}] connected")
-        timeout = time() + 30
-        while not self.done:
+        ONE_MINUTE = time() + 60
+        while not time_to_stop:
             try:
-                if time() > timeout: break
-                else:
-                    agent_request = agent_socket.recv(1024).decode()
-                    if agent_request:
-                        print(f"[FOXDIE: {agent_port}] {agent_request}")
-                        agent_socket.send("OK".encode())
-                        timeout = time() + 30
+                if time() > ONE_MINUTE: break
+                agent_request = agent_socket.recv(1024).decode()
+                if agent_request:
+                    print(f"[FOXDIE: {agent_port}] {agent_request}")
+                    agent_socket.send("OK".encode())
+                    ONE_MINUTE = time() + 60
             except BlockingIOError: pass
             except ConnectionError: break
         print(f"[FOXDIE: {agent_port}] disconnected")
 
-    def dispatch(self):
-        listener = socket(AF_INET, SOCK_STREAM)
-        listener.bind(self.address)
-        listener.listen(5)
-        listener.setblocking(False)
-        print(f"[FOXDIE: {self.port}] started listening")
-        while not self.done:
+@dataclass
+class Listener:
+    ip: IPv4Address = field(default = None, init = True)
+    port: int = field(default = None, init = True)
+    handler: Handler = field(default = None, init = True)
+
+    def start(self):
+        self.address = (str(self.ip), self.port)
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind(self.address)
+        self.socket.listen(5)
+        self.socket.setblocking(False)
+        print(f"[FOXDIE: 80] started listening")
+        while not time_to_stop:
             try:
-                agent_socket, agent_address = listener.accept()
+                agent_socket, agent_address = self.socket.accept()
                 agent_port = agent_address[1]
                 thread = Thread(
                     name = agent_port,
-                    target = self.handler,
+                    target = self.handler.start,
                     args = [agent_socket, agent_port]
                 )
                 thread.start()
             except BlockingIOError: pass
-        listener.close()
-        print(f"[FOXDIE: {self.port}] stopped listening")
+        self.socket.close()
+        print(f"[FOXDIE: 80] stopped listening")
+
+@dataclass
+class Server:
+    listener: Listener = field(default = None, init = True)
 
     def start(self):
-        foxdie = Thread(target = self.dispatch)
-        foxdie.start()
+        c2 = Thread(target = self.listener)
+        c2.start()
         try:
-            while not self.done: sleep(0.5)
+            while not time_to_stop: 
+                sleep(0.5)
         except KeyboardInterrupt: 
-            self.done = True
-        foxdie.join()
+            time_to_stop = True
+            c2.join()
 
 def main():
+    listener = Listener(ip = "127.0.0.1", port = 80, handler = Handler)
     server = Server()
     server.start()
 
